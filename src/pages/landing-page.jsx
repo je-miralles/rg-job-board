6acbbeb40c317ff56c7f8565e5e3b0ffe28f7831
Emilio Miralles <emilio_miralles@sfu.ca>
2023-03-24 15:36:55 +0100

    Import useState to save the external data.

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,15 +1,19 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import { csv } from "d3-fetch";
 
 export default function Landing_Page() {
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQTIAE4rAi3VwxQ3zSHlBgZNg43gfMVJ-uYiXFDvdDHNQMYPTNyir155Vbv2o2KacdYb8BZSSIJI88A/pub?gid=0&single=true&output=csv";
 
         const fetchData = async () => {
             try {
-                const theCsv = await csv(url)
-                console.log(theCsv);
+                const theCsv = await csv(url);
+                theCsv.map((value, key) => {
+                    console.log("key:", key, "value:", value.title);
+                });
+                setData(theCsv);
             } catch (error) {
                 console.log("error", error);
             }
@@ -18,5 +22,16 @@ export default function Landing_Page() {
         fetchData();
     }, []);
 
-    return <h1>Hello, landing page</h1>;
+    return(
+        <div className="container">
+            <h1>Hello, landing page</h1>
+            <div className="listings">
+                {Object.entries(data).map((listing, key) => (
+                    <div className="item" key={key}>
+                        title {key}:  {listing.title}
+                    </div>
+                ))}
+             </div>
+        </div>
+    );
 }
