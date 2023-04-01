0f0fbb87656d2f652394b942e946e32f5baaf47d
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-02 00:37:06 +0200

    Fix prefix.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <title>ListingBoard</title>
         <meta name="description" content="A board of listings" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="{ prefix }/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <LandingPage url={ url }/>
     </>
