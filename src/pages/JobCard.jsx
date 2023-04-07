fe631c63169b9984310712052983172ccc387ac1
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-07 19:56:39 +0200

    Add defaultProps for JobCard data.

--- src/pages/JobCard.jsx
+++ src/pages/JobCard.jsx
@@ -1,6 +1,20 @@
-import { React } from 'react'
+import { React } from 'react';
 import { Card, Text, Group } from '@mantine/core';
 
 export default function JobCard({data}) {
     return <Card>{data.title}</Card>
 }
+
+JobCard.defaultProps = {
+    data: {
+        'title': "",
+        'description': "",
+        'contact': "",
+        'time-committment': "",
+        'minimum commitment': "",
+        'start date': "",
+        'number of people to fill this role': "",
+        'applicant-characteristics': "",
+        'Link to more details': ""
+    }
+};
