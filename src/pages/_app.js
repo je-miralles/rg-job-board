0ae6a15abecd4a2fa487764a5b16c96ab6c71764
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-19 20:07:08 +0200

    Move Head to app.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,30 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }) {
 
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "light",
-        colors: {
-          // 'body-background': '#F8F8F8', // Doesn't work
-        }, }}
-    >
-      <Component {...pageProps} />
-    </MantineProvider>
-  )
+    <>
+      <Head>
+        <title>RG Job Board</title>
+        <link rel="shortcut icon" href="favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+          colors: {
+            // 'body-background': '#F8F8F8', // Doesn't work
+          }, }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,11 @@
-import Head from 'next/head';
 import { Group } from "@mantine/core";
 
 import LandingPage from './LandingPage';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>ListingBoard</title>
-        <meta name="description" content="A board of listings" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Group mt={50} position="center">
       <LandingPage />
-    </>
-  )
+    </Group>
+  );
 }
