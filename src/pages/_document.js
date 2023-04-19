a57b67ad1e8a40019ae8e250906bb7c61e88e361
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-19 20:11:44 +0200

    Bring back next custom document code.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,17 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 const getInitialProps = createGetInitialProps();
 export default class _Document extends Document {
-    static getInitialProps = getInitialProps;
-}
+  static getInitialProps = getInitialProps;
 
-// export default function Document() {
-//   return (
-//     <Html lang="en">
-//       <Head />
-//       <body>
-//         <Main />
-//         <NextScript />
-//       </body>
-//     </Html>
-//   )
-// }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
