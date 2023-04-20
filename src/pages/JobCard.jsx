c9c21a876ce30e8210321e11753b1e51e69da759
alejandrokennedy <alejandrokennedy@gmail.com>
2023-04-20 13:33:51 -0700

    randomized colors for cards

--- src/pages/JobCard.jsx
+++ src/pages/JobCard.jsx
@@ -1,8 +1,15 @@
 import { Card, Text, Group } from '@mantine/core';
 
 export default function JobCard({ data }) {
-    console.log(data.link)
-    return <Card component="a" href={`${data.link}`} target='blank' radius="md" withBorder>
+
+    const randomColor = (opacity = 1) => {
+        const randVal = () => Math.min(Math.round(Math.random() * 255), 100)
+        return opacity === 1
+            ? `rgb(${randVal()}, ${randVal()}, ${randVal()})`
+            : `rgb(${randVal()}, ${randVal()}, ${randVal()}, ${opacity})`;
+    };
+
+    return <Card sx={{ backgroundColor: `${randomColor()}` }} component="a" href={`${data.link}`} target='blank' radius="md">
         <Text weight={500} size="lg" mb="md">{data.title}</Text>
         <Group position="apart">
             <Text size="sm" color="dimmed" lineClamp={10}>
@@ -13,15 +20,15 @@ export default function JobCard({ data }) {
 }
 
 JobCard.defaultProps = {
-  data: {
-    'title': "",
-    'description': "",
-    'contact': "",
-    'time-committment': "",
-    'minimum-commitment': "",
-    'start-date': "",
-    'number-of-people-to-fill-this-role': "",
-    'applicant-characteristics': "",
-    'link': ""
-  }
+    data: {
+        'title': "",
+        'description': "",
+        'contact': "",
+        'time-committment': "",
+        'minimum-commitment': "",
+        'start-date': "",
+        'number-of-people-to-fill-this-role': "",
+        'applicant-characteristics': "",
+        'link': ""
+    }
 };
