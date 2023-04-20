2cdc0f7cf07cb95242890e7cba21033dbeab8125
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-20 16:14:51 +0200

    Remove unneeded Group from index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
-import { Group } from "@mantine/core";
-
 import LandingPage from './LandingPage';
 
 export default function IndexPage() {
-  return (
-    <Group mt={50} position="center">
-      <LandingPage />
-    </Group>
-  );
+  return <LandingPage />
 }
