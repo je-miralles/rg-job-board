364e71fbb3606d13d73d553fc7bff252b12cbc81
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-20 17:26:47 +0200

    Update csv field names in JobCard defaultProps.

--- src/pages/JobCard.jsx
+++ src/pages/JobCard.jsx
@@ -13,15 +13,15 @@ export default function JobCard({ data }) {
 }
 
 JobCard.defaultProps = {
-    data: {
-        'title': "",
-        'description': "",
-        'contact': "",
-        'time-committment': "",
-        'minimum commitment': "",
-        'start date': "",
-        'number of people to fill this role': "",
-        'applicant-characteristics': "",
-        'Link to more details': ""
-    }
+  data: {
+    'title': "",
+    'description': "",
+    'contact': "",
+    'time-committment': "",
+    'minimum-commitment': "",
+    'start-date': "",
+    'number-of-people-to-fill-this-role': "",
+    'applicant-characteristics': "",
+    'link': ""
+  }
 };
